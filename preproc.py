143d6cd675b4c2bb68cc5833a3dbb739c91ec394
Rogers F Silva <rogers.silva@datalyticsolutions.com>
2017-12-12 12:58:17 -0500

    Modified preproc to run BET

--- preproc.py
+++ preproc.py
@@ -15,10 +15,34 @@ import datetime
 import shutil
 
 def prepro(basedir):
-    print('Hello, data in path: '+basedir)
+    input=glob.glob(os.path.join(basedir,'sub-*','func','sub-*task-bart_bold.nii.gz'))
+    for item in input:
+        output=item.strip('.nii.gz')+'_brain'
+        os.system('bet %s %s -F'%(item,output))
+        print('Done')
     
 def main():
-    basedir='/home/rsilva/repos/code/nc-wkshop/fmri_workshop/data'
+    basedir='/home/rsilva/repos/code/nc-wkshop/data'
+    
+    input=glob.glob(basedir+'/sub-*/func/sub-*task-bart_bold.nii.gz')
+    #print(input[0:10])
+    
+    x=input[1]
+    
+    sub=input[1].split('/')[7] # 7 is subject number
+    #print(input[1])
+    #print(sub)
+    
+    subtask=input[1].split('/')[9].split('.')[0] # 9 is file name
+    #print(subtask)
+    
+    #output=subtask+'_brain'
+    
+    output=input[1].strip('.nii.gz')+'_brain'
+    #os.system('bet %s %s -F'%(x,output))
+    
     prepro(basedir)
     
 main()
+
+#%run onset_parser.py -task bart -cols action_CASHOUT action_ACCEPT action_EXPLODE
